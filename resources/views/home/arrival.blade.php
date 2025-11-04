41619888b56cc3166488c53aae5581f36caed43b
mkOlawale <abeebolaking1@gmail.com>
2025-11-03 20:34:28 -0800

    updating

--- resources/views/home/arrival.blade.php
+++ resources/views/home/arrival.blade.php
@@ -8,14 +8,14 @@
                   <div class="col-md-6 ml-auto">
                      <div class="heading_container remove_line_bt">
                         <h2>
-                           #NewArrivals
+                           #LatestArrivals
                         </h2>
                      </div>
                      <p style="margin-top: 20px;margin-bottom: 30px;">
                         Welcome to the Cybershop we are the best and the most reliable, Here's the new arrival of our latest product. Order your own now for the best offer.
                      </p>
                      <a href="">
-                     Shop Now
+                         Shop Now
                      </a>
                   </div>
                </div>
